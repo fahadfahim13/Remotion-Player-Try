6a87142744e9c34ca92c5a614dda59fd4e240fa8
Fahim <fahim.hasnain@ixorasolution.com>
2021-09-26 17:13:09 +0600

    .riv file added

--- src/components/Components/VideoComponent.tsx
+++ src/components/Components/VideoComponent.tsx
@@ -2,19 +2,37 @@ import React from 'react'
 import video from '../../static/1.mp4'
 import { Video } from 'remotion'
 import { AnimateKeyframes } from 'react-simple-animate'
-import {Animated} from "react-animated-css";
+import Rive from 'rive-react';
+import { useRive, useStateMachineInput } from 'rive-react';
+// import {Animated} from "react-animated-css";
+// import basketball from '../../static/basketball.riv'
+
 
 
 function VideoComponent(props: {playVideo: boolean}) {
 
     const { playVideo } = props
+    const STATE_MACHINE_NAME = 'button';
+    const INPUT_NAME = 'onClick';
+
+    const { RiveComponent, rive } = useRive({
+        src: "../../static/basketball.riv",
+        stateMachines: STATE_MACHINE_NAME,
+        autoplay: true,
+    });
+    const onClickInput = useStateMachineInput(
+        rive,
+        STATE_MACHINE_NAME,
+        INPUT_NAME
+    );
 
     return (
         <AnimateKeyframes
             play={playVideo}
             pause={!playVideo}
             iterationCount="infinite"
-            direction="alternate-reverse"
+            // direction="alternate-reverse"
+            direction="alternate"
             duration={0.3}
             keyframes={[
                 'transform: rotateX(0) rotateY(-90deg) rotateZ(0)',
@@ -25,11 +43,13 @@ function VideoComponent(props: {playVideo: boolean}) {
             <Video
                 src={video}
                 startFrom={0} // if video is 30fps, then it will start at 2s
-                endAt={30 * 10} // if video is 30fps, then it will end at 4s
+                endAt={30 * 2} // if video is 30fps, then it will end at 4s
                 style={{width: '100%'}}
                 // style={{...style, width: '100%'}}
                 disableRemotePlayback
             />
+            <RiveComponent onClick={() => onClickInput?.fire()} />
+
          {/* </Animated> */}
         </AnimateKeyframes>
     )
